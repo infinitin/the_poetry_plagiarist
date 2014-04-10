c28455abd10f0225811a53469c409c293c4c7115
Nitin <git@nitinbn.com>
2014-04-11 00:18:04 +0800

    Now we're getting somewhere

--- shayar/generalise/__main__.py
+++ shayar/generalise/__main__.py
@@ -1,6 +1,13 @@
 __author__ = 'Nitin'
 import web
 from web import form
+from aggregate import generalise
+from shayar.poem_template import Template
+# This import is required for the pickle load
+# noinspection PyUnresolvedReferences,PyPep8Naming
+import shayar.poem as poem
+import os
+import cPickle
 
 render = web.template.render('templates/')
 
@@ -8,37 +15,46 @@ urls = ('/', 'index')
 app = web.application(urls, globals())
 
 settings_form = form.Form(
-    form.Dropdown('Number of stanzas', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Lines per stanza', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Locations of repeated lines', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Number of repeated lines', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Number of distinct sentences', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Tenses of each line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Overall tense', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Assonance', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Consonance', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Alliteration', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Rhyme', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Syllables per line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Rhythm', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Similes', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Number of characters', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Genders of each character', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Singular/Plural for each character', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Animation of each character', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Personification for each character', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Relations for each character', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Distribution of relations over characters', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Phrases to include in each line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Phrases to include in the poem', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Topics', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Modality by line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Polarity by line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Subjectivity by line', ['mustard', 'fries', 'wine']),
-    form.Dropdown('Mood by line', ['mustard', 'fries', 'wine']),
+    form.Dropdown('collection', []),
+    form.Dropdown('stanzas', []),
+    form.Dropdown('num_lines', []),
+    form.Dropdown('repeated_lines_locations', []),
+    form.Dropdown('num_repeated_lines', []),
+    form.Dropdown('num_distinct_sentences', []),
+    form.Dropdown('line_tenses', []),
+    form.Dropdown('overall_tense', []),
+    
+    form.Dropdown('assonance', []),
+    form.Dropdown('consonance', []),
+    form.Dropdown('alliteration', []),
+    
+    form.Dropdown('rhyme_schemes', []),
+    form.Dropdown('syllable_patterns', []),
+    form.Dropdown('stress_patterns', []),
+    
+    form.Dropdown('Similes', []),
+    
+    form.Dropdown('character_count', []),
+    form.Dropdown('character_genders', []),
+    form.Dropdown('character_nums', []),
+    form.Dropdown('character_animations', []),
+    form.Dropdown('character_personifications', []),
+    form.Dropdown('character_relations', []),
+    form.Dropdown('character_relation_distribution', []),
+    
+    form.Dropdown('n_grams_by_line', []),
+    form.Dropdown('n_grams', []),
+    form.Dropdown('hypernym_ancestors', []),
+    
+    form.Dropdown('polarity_by_line', []),
+    form.Dropdown('subjectivity_by_line', []),
+    form.Dropdown('modality_by_line', []),
+    form.Dropdown('mood_by_line', []),
+
     form.Checkbox('Plot'),
     form.Checkbox('Persist')
 )
+
 
 class index:
     def __init__(self):
@@ -46,20 +62,40 @@ class index:
 
     def GET(self):
         settings = settings_form()
-        # make sure you create a copy of the form by calling it (line above)
-        # Otherwise changes will appear globally
-        return render.formtest(settings)
+        return render.feature_settings(settings)
 
     def POST(self):
         settings = settings_form()
         if not settings.validates():
-            return render.formtest(settings)
+            return render.feature_settings(settings)
         else:
-            # form.d.boe and form['boe'].value are equivalent ways of
-            # extracting the validated arguments from the form.
-            return "Grrreat success! boe: %s, bax: %s" % (settings.d.boe, settings['bax'].value)
+            # Should probably send this off to another thread so not to leave the request hanging.
+            #  The return will never come through!
+            template = Template(settings)
+            poems = apply_givens(retrieve_all_poems(template.collection), template)
+            generalise(template, poems, settings['Plot'].value, settings['Persist'].value)
+            return "All done!"    # Render the plots if plot
 
 
 if __name__ == "__main__":
-    #web.internalerror = web.debugerror
+    web.internalerror = web.debugerror
     app.run()
+    
+
+#Given the current set of applicable poems and the (new) givens, filter the poems accordingly.
+#The keys of the givens correspond exactly to the attributes of the poem template object.
+def apply_givens(poems, givens):
+    #Run some list comprehensions in some nice way
+    for attr in givens:
+        # Not quite but you get the point:
+        poems = [poem for poem in poems if givens[attr].value in getattr(poem, attr)]
+
+    return poems
+
+
+#Grab all the poems of a particular collection from store
+def retrieve_all_poems(collection):
+    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..\\analyse', collection + '.poems')), 'rb')
+    poems = cPickle.load(f)
+    f.close()
+    return poems
--- shayar/generalise/aggregate.py
+++ shayar/generalise/aggregate.py
@@ -1,12 +1,7 @@
 __author__ = 'Nitin'
-import os
 import cPickle
 from threading import Thread
-# This import is required for the pickle load
-# noinspection PyUnresolvedReferences,PyPep8Naming
-import shayar.poem as poem
-from shayar.poem_template import Template
-import argparse
+
 from aggregators.basic_structure import agg_n_stanzas, agg_lines_per_stanza, agg_repeated_line_locations, \
     agg_n_repeated_lines, agg_n_distinct_sentences, agg_line_tenses, agg_overall_tense
 from aggregators.line_patterns import agg_assonance, agg_consonance, agg_alliteration
@@ -20,13 +15,9 @@ from aggregators.semantics import agg_character_hypernyms, agg_modality_by_line,
     agg_subjectivity_by_line, agg_mood_by_line
 
 
-def generalise(preset, plot):
-    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..\\analyse', preset + '.poems')), 'rb')
-    poems = cPickle.load(f)
-    f.close()
-
-    template = Template(preset)
-
+#Given a set of poems, fill up the template with options
+#Plot graphs and persist if necessary
+def generalise(template, poems, plot, persist):
     aggregators = [agg_n_stanzas, agg_lines_per_stanza, agg_repeated_line_locations, agg_n_repeated_lines,
                    agg_n_distinct_sentences, agg_line_tenses, agg_overall_tense, agg_assonance, agg_consonance,
                    agg_alliteration, agg_rhyme, agg_syllable, agg_rhythm, agg_similes, agg_character_count,
@@ -47,32 +38,10 @@ def generalise(preset, plot):
     if plot:
         template.plot('')
 
-
-def apply_givens(poems):
-    pass
-    #Take givens from builder/web interface/command line interface
-    #givens = []
-    #Run some list comprehensions in some nice way
-    #for attr, value in givens:
-        # For most cases:
-    #    poems = [poem for poem in poems if value in getattr(poem, attr)]
-
-    #return poems
-
-
-def command_line_parser():
-    arg_parser = argparse.ArgumentParser(description='Gather insight on poems.')
-
-    arg_parser.add_argument('collection', choices=['test'], help='The particular collection of poems to be analysed.')
-
-    arg_parser.add_argument('-plot', '--plot', action='store_true',
-                            help='If true, shows a plot of every attribute. False by default.')
-
-    arg_parser.add_argument('-persist', '--persist', action='store_true',
-                            help='If true, serialise the results for later use. False by default.')
-
-    #parser.add_argument('-nstanzas', type=int, choices=[1, 2, 3], help='Fix the number of stanzas in the poem')
-
-    return arg_parser
+    if persist:
+        out = open(template.collection+'.template', 'wb+')
+        out.truncate()
+        cPickle.dump(template, out, -1)
+        out.close()
 
 
